d2fac8d56fae9ccc356cb78ff5659a8c7153c1b5
e1b23017 <e1b23017@oit.ac.jp>
2025-10-07 16:32:44 +0900

    sample2-2完了

--- src/main/java/oit/is/team2/kaizi/keisan/controller/Sample21Controller.java
+++ src/main/java/oit/is/team2/kaizi/keisan/controller/Sample21Controller.java
@@ -22,4 +22,29 @@ public class Sample21Controller {
   public String sample21() {
     return "sample21.html";
   }
+
+  /**
+   * パスパラメータ2つをGETで受け付ける 1つ目の変数をparam1という名前で，2つ目の変数をparam2という名前で受け取る
+   * GETで受け取った2つの変数とsample22()の引数の名前が同じなため， 引数の前に @PathVariable と付けるだけで，パスパラメータの値を
+   * javaで処理できるようになる ModelMapはthymeleafに渡すためのMapと呼ばれるデータ構造を持つ変数
+   * Mapはkeyとvalueの組み合わせで値を保持する
+   *
+   * @param param1
+   * @param param2
+   * @param model
+   * @return
+   */
+  @GetMapping("/sample22/{param1}/{param2}")
+  public String sample22(@PathVariable String param1, @PathVariable String param2, ModelMap model) {
+    int tasu = Integer.parseInt(param1);// param1が文字列なので，parseIntでint型の数値に変換する
+    int tasareru = Integer.parseInt(param2);
+    int tasuResult = tasu + tasareru;
+
+    // ModelMap型変数のmodelにtasuResult1という名前の変数で，tasuResultの値を登録する．
+    // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
+    model.addAttribute("tasuResult1", tasuResult);
+    return "sample21.html";
+
+  }
+
 }
