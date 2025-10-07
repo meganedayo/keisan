736f7a7795a7bd1ec29e0600980580a5ef63d863
meganedayo <e1b23021@oit.ac.jp>
2025-10-07 14:22:25 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team2/kaizi/keisan/KeisanApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team2.kaizi.keisan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeisanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
